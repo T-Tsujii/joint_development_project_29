6f28e3ad4e3efa5a15334106b312b5c5ffe9b8a6
shogo2421 <2421falsecarp@gmail.com>
2020-08-16 14:23:28 +0900

    edit, updateアクション作成。new, showビューのレイアウト微調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,15 @@ def show
     @user = User.find(params[:id])
 end
 
+def edit
+    @user = User.find(params[:id])
+end
+
+def update
+    user = User.find(params[:id])
+    user.update(user_params)
+end
+
 
 
 
