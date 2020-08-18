e0e5910184d529762075b16dedb0005d5818d801
jylee2020 <66097124+jylee2020@users.noreply.github.com>
2020-08-18 14:41:13 +0900

    new,createアクション作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # Strong Parameter の記述
+    Task.create(task_params)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
 end
